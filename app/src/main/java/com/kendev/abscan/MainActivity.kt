8d82bde8d5604efa295c84d340f37db036982ab9
kennn21 <91940623+kennn21@users.noreply.github.com>
2022-10-04 23:52:18 +0700

    - Implemented QR Scanner - Firebase installed

--- app/src/main/java/com/kendev/abscan/MainActivity.kt
+++ app/src/main/java/com/kendev/abscan/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.kendev.abscan
 
+import android.app.AlertDialog
+import android.app.SearchManager
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scanButton:LinearLayout = findViewById(R.id.button_checkin)
+        scanButton.setOnClickListener({
+            val intentIntegrator = IntentIntegrator(this)
+
+            intentIntegrator.setDesiredBarcodeFormats(listOf(IntentIntegrator.QR_CODE))
+            intentIntegrator.initiateScan()
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var result = IntentIntegrator.parseActivityResult(resultCode, data)
+        if (result != null){
+            AlertDialog.Builder(this)
+                .setMessage("Would you like to go to ${result.contents}?")
+                .setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
+                    val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                    intent.putExtra(SearchManager.QUERY,result.contents)
+                    startActivity(intent)
+                })
+                .setNegativeButton("No",DialogInterface.OnClickListener { dialogInterface, i ->  })
+                .create()
+                .show()
+        }
     }
 }
