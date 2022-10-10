2313a151b66cc62989b14e726bc4167486050ccc
kennn21 <91940623+kennn21@users.noreply.github.com>
2022-10-10 22:39:14 +0700

    - Rombak layout main - added scheduleActivity - Added function/logic for date&time attendance check to class

--- app/src/main/java/com/kendev/abscan/MainActivity.kt
+++ app/src/main/java/com/kendev/abscan/MainActivity.kt
@@ -1,22 +1,20 @@
 package com.kendev.abscan
 
-import android.app.AlertDialog
-import android.app.SearchManager
-import android.content.DialogInterface
 import android.content.Intent
-import android.os.Build.VERSION_CODES.P
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.libraries.places.api.model.LocalTime
 import com.google.firebase.auth.FirebaseAuth
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.zxing.integration.android.IntentIntegrator
 import com.kendev.abscan.others.SharedVariables
-import kotlinx.coroutines.tasks.await
-import java.lang.Exception
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +34,12 @@ class MainActivity : AppCompatActivity() {
         button_signout.setOnClickListener(){
             FirebaseAuth.getInstance().signOut();
             val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        val button_check_schedules: LinearLayout = findViewById(R.id.button_check_schedules)
+        button_check_schedules.setOnClickListener(){
+            val intent = Intent(this, SchedulesActivity::class.java)
             startActivity(intent)
         }
 
@@ -59,28 +63,65 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun doAttendance(code:String){
+    private fun getDateTime(): String? {
+        val dateFormat: DateFormat = SimpleDateFormat("EEEE HH:mm:ss")
+        val date = Date()
+        return dateFormat.format(date)
+    }
+
+    private fun doAttendance(code:String) {
         val db = Firebase.firestore
         val classes = db.collection("classes")
 
         classes.get()
             .addOnSuccessListener { result ->
                 for (cls in result) {
-                    if(cls.data["code"] == code){
-                        var className = cls.data["name"].toString()
-                        Log.d("CLASSNAME", className)
-                        SharedVariables().changeName(className)
-                        val intent = Intent(this, presentActivity::class.java)
-                        intent.putExtra("className", className)
-                        startActivity(intent)
+                    var dateAndTime = getDateTime()
+                    var convDate = dateAndTime.toString().split("\\s".toRegex()).toTypedArray()
+                    if (
+                        cls.data["code"] == code
+                    ) {
+                        if(
+                            cls.data["day"].toString().trim() == convDate[0].trim()
+                        ){
+                            val d1 = convDate[1]
+                            val d2 = cls.data["time_start"].toString().trim()
+                            val d3 = cls.data["time_end"].toString().trim()
 
-                    }
-//                    AlertDialog.Builder(this)
+                            val sdf = SimpleDateFormat("HH:mm:ss")
+
+                            val currTime: Date = sdf.parse(d1)
+                            val startTime: Date = sdf.parse(d2)
+                            val endTime: Date = sdf.parse(d3)
+                            if(
+                                currTime.compareTo(startTime) > 0
+                            ){
+                                if(
+                                    currTime.compareTo(endTime) < 0
+                                ){
+                                    var className = cls.data["name"].toString()
+                                    Log.d("CLASSNAME", className)
+                                    SharedVariables().changeName(className)
+                                    val intent = Intent(this, presentActivity::class.java)
+                                    intent.putExtra("className", className)
+                                    startActivity(intent)
+                                } else{
+                                    Toast.makeText(baseContext,"Now is "+convDate[1]+". You should come before "+ cls.data["time_end"],Toast.LENGTH_LONG).show()
+                                }
+                            } else{
+                                Toast.makeText(baseContext,"Now is "+convDate[1]+". You should come after "+ cls.data["time_start"],Toast.LENGTH_LONG).show()
+                            }
+
+                        } else{
+                            Toast.makeText(baseContext,"Today is "+convDate[0]+". You should come on "+ cls.data["day"],Toast.LENGTH_LONG).show()
+                        }
+//                        AlertDialog.Builder(this)
 //                        .setMessage("${cls.id} => ${cls.data["code"]}")
-//                        .create()
-//                        .show()
+//                            .setMessage(cls.data["day"].toString())
+//                            .create()
+//                            .show()
+                    }
                 }
             }
-
     }
-}
+    }
